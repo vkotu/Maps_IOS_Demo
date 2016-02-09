d9c7b31a1c1a01aa75f8d141adbe2089fc8b19cf
venkat <vikkii89@gmail.com>
2016-02-09 14:41:10 -0800

    maps example in ios

--- Maps_IOS/ViewController.swift
+++ Maps_IOS/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //16.976642, 82.225624
+        var latitude:CLLocationDegrees = 16.976642
+        var longitude:CLLocationDegrees = 82.225624
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var map: MKMapView!
 
 }
 
